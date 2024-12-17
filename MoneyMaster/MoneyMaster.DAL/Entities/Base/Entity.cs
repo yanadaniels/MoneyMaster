20327d8cae3fed052f83827fb91eea17b93cd81a
Marat <milya010790@mail.ru>
2024-12-17 10:46:21 +0500

    Базовая реализация первичного ключа

---
+++ MoneyMaster/MoneyMaster.DAL/Entities/Base/Entity.cs
@@ -0,0 +1,14 @@
+﻿using MoneyMaster.DAL.Interfaces;
+
+namespace MoneyMaster.DAL.Entities.Base
+{
+    /// <summary>Сущность</summary>
+    /// <typeparam name="TKey">Тип первичного ключа</typeparam>
+    public abstract class Entity<TKey> : IEntity<TKey>
+    {
+        /// <summary>Первичный ключ </summary>
+        public required TKey Id { get; set; }
+    }
+
+    public abstract class Entity : Entity<Guid> { } 
+}
