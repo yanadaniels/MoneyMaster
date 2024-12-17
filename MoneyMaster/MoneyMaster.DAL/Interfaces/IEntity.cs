298440525717a59424ceda09916f87c3a306d846
Marat <milya010790@mail.ru>
2024-12-17 10:33:17 +0500

    Интерфейс для сущностей

---
+++ MoneyMaster/MoneyMaster.DAL/Interfaces/IEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace MoneyMaster.DAL.Interfaces
+{
+    /// <summary>Сущность</summary>
+    /// <typeparam name="TKey"></typeparam>
+    public interface IEntity<TKey>
+    {
+        /// <summary>Идентификатор</summary>
+        TKey Id { get; }
+    }
+
+    /// <summary>Сущность</summary>
+    public interface IEntity : IEntity<Guid> { }
+}
