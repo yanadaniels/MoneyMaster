c419393578d5171e36fd40b923d329dc8d476060
Marat <milya010790@mail.ru>
2024-12-18 16:08:26 +0500

    Добавлены интерфейсы сущностей

---
+++ Domain/MoneyMaster.Domain.Entities/Interfaces/INamedEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace MoneyMaster.Domain.Entities
+{
+    /// <summary>Именованная сущность</summary>
+    /// <typeparam name="TKey">Тип первичного ключа</typeparam>
+    public interface INamedEntity<out TKey> : IEntity<TKey>
+    {
+        /// <summary>Имя</summary>
+        string Name { get; }
+    }
+
+    /// <summary>Именованная сущность</summary>
+    public interface INamedEntity : INamedEntity<Guid>, IEntity { }
+}
---
+++ Domain/MoneyMaster.Domain.Entities/Interfaces/ITimedEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace MoneyMaster.Domain.Entities
+{
+    /// <summary>Сущность определенная во времени</summary>
+    /// <typeparam name="TKey"></typeparam>
+    public interface ITimedEntity<out TKey> : IEntity<TKey>
+    {
+        /// <summary>Время</summary>
+        DateTime CreateAt { get; set; }
+    }
+
+    /// <summary>Сущность определенная во времени</summary>
+    public interface ITimedEntity : ITimedEntity<Guid> { }
+}
