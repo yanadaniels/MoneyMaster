4a2f958c2ccf4895769b5f94a0680e41412eb4f6
Marat <milya010790@mail.ru>
2024-12-18 16:16:41 +0500

    Перечисления пока сюда положил

---
+++ Domain/MoneyMaster.Domain.Entities/Enums/TransactionType.cs
@@ -0,0 +1,13 @@
+﻿namespace MoneyMaster.Domain.Entities.Enums
+{
+    /// <summary>Тип транзакции</summary>
+    public enum TransactionType
+    {
+        /// <summary>Доход </summary>
+        Income,
+        /// <summary>Расход</summary>
+        Expense,
+        /// <summary>Перевод</summary>
+        Transfer
+    }
+}
