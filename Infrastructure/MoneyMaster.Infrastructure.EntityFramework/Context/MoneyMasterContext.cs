d44b6a2076ecf10143df46ef049b303bcf4b85a4
Marat <milya010790@mail.ru>
2024-12-19 13:32:37 +0500

    Контекст

---
+++ Infrastructure/MoneyMaster.Infrastructure.EntityFramework/Context/MoneyMasterContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoneyMaster.Infrastructure.EntityFramework.Configurations;
+
+namespace MoneyMaster.Infrastructure.EntityFramework.Context
+{
+    /// <summary>Контекст базы данных </summary>
+    public class MoneyMasterContext : DbContext
+    {
+        public MoneyMasterContext(DbContextOptions<MoneyMasterContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.ApplyConfiguration(new AccountConfiguration());
+            builder.ApplyConfiguration(new AccountTypeConfiguration());
+            builder.ApplyConfiguration(new CategoryConfiguration());
+            builder.ApplyConfiguration(new ReportConfiguration());
+            builder.ApplyConfiguration(new TransactionConfiguration());
+            builder.ApplyConfiguration(new UserConfiguration());
+            builder.ApplyConfiguration(new UserSettingConfiguration());
+        }
+    }
+}
