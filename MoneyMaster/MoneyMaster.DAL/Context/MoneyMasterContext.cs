53f617f2d2339dc5e2639767bb32ce4c62ad9c74
Marat <milya010790@mail.ru>
2024-12-19 12:51:25 +0500

    контекст

--- MoneyMaster/MoneyMaster.DAL/Context/MoneyMasterContext.cs
+++ MoneyMaster/MoneyMaster.DAL/Context/MoneyMasterContext.cs
@@ -1,6 +1,24 @@
-﻿namespace MoneyMaster.DAL.Context
+﻿using Microsoft.EntityFrameworkCore;
+using MoneyMaster.DAL.Configurations;
+
+namespace MoneyMaster.DAL.Context
 {
-    public class MoneyMasterContext
+    /// <summary>Контекст базы данных </summary>
+    public class MoneyMasterContext : DbContext
     {
+        public MoneyMasterContext(DbContextOptions<MoneyMasterContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.ApplyConfiguration(new AccountConfiguration());
+            builder.ApplyConfiguration(new AccountTypeConfiguration());
+            builder.ApplyConfiguration(new CategoryConfiguration());
+            builder.ApplyConfiguration(new ReportConfiguration());
+            builder.ApplyConfiguration(new TransactionConfiguration());
+            builder.ApplyConfiguration(new UserConfiguration());
+            builder.ApplyConfiguration(new UserSettingConfiguration());
+        }
     }
 }
