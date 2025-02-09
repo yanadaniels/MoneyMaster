d54b1d01f18ccbd206ff682e564b954931249ac1
Marat <milya010790@mail.ru>
2025-02-09 23:34:54 +0500

    Добавляем валидатор

---
+++ IdentityService/Hosts/IdentityService.WebAPI/Validation/User/CreatingUserValidation.cs
@@ -0,0 +1,9 @@
+﻿using FluentValidation;
+using IdentityService.WebAPI.Models.User;
+
+namespace IdentityService.WebAPI.Validation.User
+{
+    public class CreatingUserValidation : AbstractValidator<CreatingUserModel>
+    {
+    }
+}
