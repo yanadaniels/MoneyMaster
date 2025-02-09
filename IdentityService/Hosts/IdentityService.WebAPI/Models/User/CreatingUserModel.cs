ab9044189803e509f23f1613a548c426a0b12015
Marat <milya010790@mail.ru>
2025-02-09 22:42:27 +0500

    Модели для API

---
+++ IdentityService/Hosts/IdentityService.WebAPI/Models/User/CreatingUserModel.cs
@@ -0,0 +1,24 @@
+﻿namespace IdentityService.WebAPI.Models.User
+{
+    /// <summary>Модель создания пользователя</summary>
+    public class CreatingUserModel
+    {
+        /// <summary>Имя пользователя</summary>
+        public required string UserName { get; set; }
+
+        /// <summary>Почтовый адрес пользователя</summary>
+        public required string Email { get; set; }
+
+        /// <summary>Пароль</summary>
+        public required string PasswordHash { get; set; }
+
+        /// <summary>Имя пользователя в телеграмм</summary>
+        public string? TelegramUserName { get; set; }
+
+        /// <summary>Роли пользователя</summary>
+        public string Role { get; set; } = "User";
+
+        /// <summary>Время</summary>
+        public DateTime CreateAt { get; set; } = DateTime.UtcNow;
+    }
+}
---
+++ IdentityService/Hosts/IdentityService.WebAPI/Models/User/UserModel.cs
@@ -0,0 +1,29 @@
+﻿namespace IdentityService.WebAPI.Models.User
+{
+    /// <summary>Модель пользователя</summary>
+    public class UserModel 
+    {
+        /// <summary>Первичный ключ </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>Имя пользователя</summary>
+        public required string UserName { get; set; }
+
+        /// <summary>Почтовый адрес пользователя</summary>
+        public required string Email { get; set; }
+
+        /// <summary>Пароль</summary>
+        public required string PasswordHash { get; set; }
+
+        /// <summary>Время</summary>
+        public DateTime CreateAt { get; set; }
+
+        /// <summary>Имя пользователя в телеграмм</summary>
+        public string? TelegramUserName { get; set; }
+
+        /// <summary>Роли пользователя</summary>
+        public required string Role { get; set; }
+    }
+
+    
+}
